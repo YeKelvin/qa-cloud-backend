ea17b4e0a9ec8e13ad72747d0a8f85d808c6f995
Kelvin.Ye <testmankelvin@163.com>
2020-06-15 14:11:38 +0800

    修改日志配置

--- server/utils/log_util.py
+++ server/utils/log_util.py
@@ -4,26 +4,69 @@
 # @Time    : 2019/11/7 10:12
 # @Author  : Kelvin.Ye
 import logging
+import logging.config
 
 from server.utils import config
 
 # 日志输出格式
-FORMATTER = logging.Formatter('[%(asctime)s][%(levelname)s][%(name)s.%(funcName)s %(lineno)d] %(message)s')
+FORMATTER = logging.Formatter(
+    '[%(asctime)s][%(levelname)s][%(threadName)s][null][%(lineno)d][%(name)s.%(funcName)s][null] %(message)s'
+)
 
 # 输出到控制台
 CONSOLE_HANDLER = logging.StreamHandler()
 CONSOLE_HANDLER.setFormatter(FORMATTER)
 
 # 写入日志文件
-FILE_HANDLER = logging.FileHandler(config.get('log', 'name'))
+LOG_FILE_NAME = config.get('log', 'name')
+FILE_HANDLER = logging.FileHandler(LOG_FILE_NAME)
 
 # 日志级别
 LEVEL = config.get('log', 'level')
 
+LOGGING_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format': FORMATTER
+        },
+    },
+    'filters': {
+
+    },
+    'handlers': {
+        'console': {
+            'level': LEVEL,
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+        'default': {
+            'level': LEVEL,
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'filename': LOG_FILE_NAME,
+            'when': 'MIDNIGHT',
+            'backupCount': 7,
+            'encoding': 'utf8',
+            'interval': 1,
+            'formatter': 'standard',
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console', 'default'],
+            'level': LEVEL,
+            'propagate': False
+        }
+    }
+}
+
+logging.config.dictConfig(config=LOGGING_CONFIG)
+
 
 def get_logger(name):
     logger = logging.getLogger(name)
-    logger.propagate = False
-    logger.setLevel(LEVEL)
-    logger.addHandler(CONSOLE_HANDLER)
+    # logger.propagate = False
+    # logger.setLevel(LEVEL)
+    # logger.addHandler(CONSOLE_HANDLER)
     return logger
