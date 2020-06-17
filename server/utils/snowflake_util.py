bdb070ff843e76f208ef193951897aa89ce4c719
Kelvin.Ye <testmankelvin@163.com>
2020-06-17 19:04:54 +0800

    update

--- server/utils/snowflake_util.py
+++ server/utils/snowflake_util.py
@@ -22,18 +22,18 @@ SEQUENCE = 0        # 序列号
 import time
 import logging
 
-# 64位ID的划分
-WORKER_ID_BITS = 5
+# 64位ID的位数划分
 DATACENTER_ID_BITS = 5
+WORKER_ID_BITS = 5
 SEQUENCE_BITS = 12
 
-# 最大取值计算
-MAX_WORKER_ID = -1 ^ (-1 << WORKER_ID_BITS)  # 2**5-1 0b11111
+# 最大取值
 MAX_DATACENTER_ID = -1 ^ (-1 << DATACENTER_ID_BITS)
+MAX_WORKER_ID = -1 ^ (-1 << WORKER_ID_BITS)  # 2**5-1 0b11111
 
-# 移位偏移计算
-WOKER_ID_SHIFT = SEQUENCE_BITS
+# 移位偏移值
 DATACENTER_ID_SHIFT = SEQUENCE_BITS + WORKER_ID_BITS
+WOKER_ID_SHIFT = SEQUENCE_BITS
 TIMESTAMP_LEFT_SHIFT = SEQUENCE_BITS + WORKER_ID_BITS + DATACENTER_ID_BITS
 
 # 序号循环掩码
@@ -41,8 +41,6 @@ SEQUENCE_MASK = -1 ^ (-1 << SEQUENCE_BITS)
 
 # Twitter元年时间戳
 TWEPOCH = 1288834974657
-
-logger = logging.getLogger('flask.app')
 
 
 class InvalidSystemClock(Exception):
@@ -52,16 +50,13 @@ class InvalidSystemClock(Exception):
 
 
 class IdWorker(object):
-    """
-    用于生成IDs
-    """
-
     def __init__(self, datacenter_id, worker_id, sequence=0):
         """
-        初始化
-        :param datacenter_id: 数据中心（机器区域）ID
-        :param worker_id: 机器ID
-        :param sequence: 其实序号
+
+        Args:
+            datacenter_id:  数据中心ID
+            worker_id:      机器ID
+            sequence:       序号号
         """
         # sanity check
         if worker_id > MAX_WORKER_ID or worker_id < 0:
@@ -76,21 +71,14 @@ class IdWorker(object):
 
         self.last_timestamp = -1  # 上次计算的时间戳
 
-    def _gen_timestamp(self):
-        """
-        生成整数时间戳
-        :return:int timestamp
-        """
-        return int(time.time() * 1000)
-
     def get_id(self):
+        """获取新ID
         """
-        获取新ID
-        :return:
-        """
-        timestamp = self._gen_timestamp()
+        timestamp = int(time.time() * 1000)
 
         # 时钟回拨
+        # 雪花算法需要是强依赖时间，如果时间发生回拨，有可能会生成重复的ID
+        # 用当前时间和上一次的时间进行判断，如果当前时间小于上一次的时间那么就发生了时间回拨，直接抛出异常
         if timestamp < self.last_timestamp:
             logging.error('clock is moving backwards. Rejecting requests until {}'.format(self.last_timestamp))
             raise InvalidSystemClock
@@ -98,23 +86,26 @@ class IdWorker(object):
         if timestamp == self.last_timestamp:
             self.sequence = (self.sequence + 1) & SEQUENCE_MASK
             if self.sequence == 0:
-                timestamp = self._til_next_millis(self.last_timestamp)
+                timestamp = self.__wait_until_next_millis()
         else:
             self.sequence = 0
 
         self.last_timestamp = timestamp
 
-        new_id = ((timestamp - TWEPOCH) << TIMESTAMP_LEFT_SHIFT) | (self.datacenter_id << DATACENTER_ID_SHIFT) | \
-                 (self.worker_id << WOKER_ID_SHIFT) | self.sequence
+        new_id = (
+                ((timestamp - TWEPOCH) << TIMESTAMP_LEFT_SHIFT) |
+                (self.datacenter_id << DATACENTER_ID_SHIFT) |
+                (self.worker_id << WOKER_ID_SHIFT) |
+                self.sequence
+        )
         return new_id
 
-    def _til_next_millis(self, last_timestamp):
+    def __wait_until_next_millis(self):
+        """等待到下一毫秒
         """
-        等到下一毫秒
-        """
-        timestamp = self._gen_timestamp()
-        while timestamp <= last_timestamp:
-            timestamp = self._gen_timestamp()
+        timestamp = int(time.time() * 1000)
+        while timestamp <= self.last_timestamp:
+            timestamp = int(time.time() * 1000)
         return timestamp
 
 
