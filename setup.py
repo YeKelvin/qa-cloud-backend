c62f09d62bebf0dfed528985d1d9042b1ca70a66
Kelvin.Ye <testmankelvin@163.com>
2020-06-28 14:11:28 +0800

    update

---
+++ setup.py
@@ -0,0 +1,36 @@
+import pathlib
+
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+setup(
+    name='test-platform-server',
+    version='0.1',
+    description='',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='',
+    license='',
+    author='Kaiwen.Ye',
+    author_email='',
+    packages=find_packages(exclude=("test",)),
+    include_package_data=True,
+    install_requires=[
+        "grpcio",
+        "grpcio-tools",
+        "requests",
+    ],
+    extras_require={[
+
+    ]},
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ]
+)
