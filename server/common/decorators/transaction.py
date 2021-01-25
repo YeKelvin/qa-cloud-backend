f1c4e02220bb317a421dec6b7325fc7bddac3954
Kelvin.Ye <testmankelvin@163.com>
2021-01-25 11:41:04 +0800

    fix: 修复装饰器bug

--- server/common/decorators/transaction.py
+++ server/common/decorators/transaction.py
@@ -3,13 +3,11 @@
 # @File    : transaction
 # @Time    : 2020/3/20 15:50
 # @Author  : Kelvin.Ye
-import traceback
 from functools import wraps
 
-from flask import request, g
-
-from server.extension import db
+from flask import g, request
 from server.common.utils.log_util import get_logger
+from server.extension import db
 
 log = get_logger(__name__)
 
@@ -24,14 +22,10 @@ def db_transaction(func):
         try:
             result = func(*args, **kwargs)
             db.session.commit()
+            return result
         except Exception:
-            log.error(
-                f'logId:[ {g.logid} ] method:[ {request.method} ] path:[ {request.path} ] '
-                f'traceback:[ {traceback.format_exc()} ]'
-            )
+            log.error(f'logId:[ {g.logid} ] method:[ {request.method} ] path:[ {request.path} ] database rollback')
             db.session.rollback()
             raise  # 重新抛出给 @http_service
-        finally:
-            return result
 
     return wrapper
