5b37742a6df6ea831bbdf28dfeac2a3f8ca18d77
Kelvin.Ye <testmankelvin@163.com>
2021-02-02 20:23:33 +0800

    🦄refactor(tests): 指定pytest搜索范围

--- server/script/controllers/test_controller.py
+++ server/script/controllers/test_controller.py
@@ -14,14 +14,6 @@ log = get_logger(__name__)
 @blueprint.route('/test/run', methods=['GET', 'POST'])
 def test_run():
     req = JsonParser(
-        Argument('elementNo'),
-        Argument('elementName'),
-        Argument('elementComments'),
-        Argument('elementType'),
-        Argument('enabled'),
-        Argument('workspaceNo'),
-        Argument('workspaceName'),
-        Argument('page', type=int, required=True, nullable=False, help='页数不能为空'),
-        Argument('pageSize', type=int, required=True, nullable=False, help='每页总数不能为空'),
+        Argument('elementNo')
     ).parse()
     return service.test_run(req)
