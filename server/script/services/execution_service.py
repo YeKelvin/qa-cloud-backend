47e1eb3077023c275393e3353675f335e17c49b1
Kelvin.Ye <testmankelvin@163.com>
2021-02-07 18:25:47 +0800

    ✨feat: 添加执行脚本的接口

--- server/script/services/execution_service.py
+++ server/script/services/execution_service.py
@@ -7,7 +7,7 @@ from server.common.decorators.service import http_service
 from server.common.request import RequestDTO
 from server.common.utils.log_util import get_logger
 from server.script.services import execution_helper as helper
-
+from sendanywhere.runner import Runner
 
 log = get_logger(__name__)
 
@@ -16,6 +16,6 @@ log = get_logger(__name__)
 def execute_script(req: RequestDTO):
     # 根据collectionNo递归查询脚本数据并转换成dict
     script = [helper.element_to_dict(req.attr.collectionNo)]
-    
 
-    pass
+    # 开始执行脚本
+    Runner.start(script)
