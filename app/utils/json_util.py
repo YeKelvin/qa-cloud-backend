6ad00133d69abf396d52bc75fe7c135a05d3b520
Kelvin.Ye <testmankelvin@163.com>
2021-09-14 09:56:52 +0800

    ✨ feat: orjson增加异常信息输出

--- app/utils/json_util.py
+++ app/utils/json_util.py
@@ -5,24 +5,31 @@
 # @Author  : Kelvin.Ye
 import orjson
 from jsonpath import jsonpath
+from orjson import JSONDecodeError
+
+from app.utils.log_util import get_logger
+
+
+log = get_logger(__name__)
 
 
 def to_json(obj):
-    """序列化
-    """
+    """序列化"""
     return orjson.dumps(obj)
 
 
-def from_json(json_text):
-    """反序列化
-    """
-    return orjson.loads(json_text)
+def from_json(val):
+    """反序列化"""
+    try:
+        return orjson.loads(val)
+    except JSONDecodeError as e:
+        e.args = e.args + (f'value:[ {val} ]',)
+        raise e
 
 
-def extract_json(json_text: str, json_path: str):
-    """根据 JsonPath提取字段值
-    """
-    result_list = jsonpath(from_json(json_text), json_path)
+def extract_json(val: str, json_path: str):
+    """根据 JsonPath提取字段值"""
+    result_list = jsonpath(from_json(val), json_path)
     if len(result_list) == 1:
         return result_list[0]
     return result_list
