39e2dbe60e4609a804b97bef14146526cd6a4e99
Kelvin.Ye <testmankelvin@163.com>
2022-04-18 22:44:47 +0800

    🦄 refactor: update TSystemOperationLogContent

--- app/system/model.py
+++ app/system/model.py
@@ -26,8 +26,6 @@ class TSystemOperationLog(DBModel, BaseColumn):
     """操作日志表"""
     __tablename__ = 'SYSTEM_OPERATION_LOG'
     LOG_NO = db.Column(db.String(32), index=True, nullable=False, comment='日志编号')
-    OPERATOR_IP = db.Column(db.String(64), comment='IP地址')
-    OPERATION_TYPE = db.Column(db.String(32), comment='操作类型(INSERT:新增, UPDATE:修改, DELETE:删除)')
     OPERATION_NAME = db.Column(db.String(128), comment='权限名称')
     OPERATION_METHOD = db.Column(db.String(128), comment='操作方法')
     OPERATION_ENDPOINT = db.Column(db.String(256), comment='操作路由')
@@ -37,8 +35,9 @@ class TSystemOperationLogContent(DBModel, BaseColumn):
     """操作日志内容表"""
     __tablename__ = 'SYSTEM_OPERATION_LOG_CONTENT'
     LOG_NO = db.Column(db.String(32), index=True, nullable=False, comment='日志编号')
+    OPERATION_TYPE = db.Column(db.String(32), nullable=False, comment='操作类型(INSERT:新增, UPDATE:修改, DELETE:删除)')
     TABLE_NAME = db.Column(db.String(128), comment='列名')
     COLUMN_NAME = db.Column(db.String(128), comment='列名')
-    COLUMN_VALUE = db.Column(db.Text, comment='列值')
     OLD_VALUE = db.Column(db.Text, comment='旧值')
     NEW_VALUE = db.Column(db.Text, comment='新值')
+    ROW_ID = db.Column(db.Integer, comment='新增行或删除行的ID')
